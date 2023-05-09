4887ae7d3d5954ae1bae705f361b3173ede0891b
“kikuchimy” <m.kikuchi@next-revolution.net>
2023-05-09 10:17:41 +0900

    一通り作成

---
+++ app/Http/Requests/StoreCommentRequest.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreCommentRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        // 今回は使用しないのでtrueを返す
+        // return false;
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            //
+            'body' => 'required|string|max:200',
+        ];
+    }
+
+    public function attributes()
+    {
+        return [
+            'body' => 'コメント',
+        ];
+    }
+}
